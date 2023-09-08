8cb66f9e887e9b17da1f2dabe5f2c5439f79a679
geeegrace <nichakorn.cha@ku.th>
2023-09-08 22:49:19 +0700

    modify views code #2

--- polls/views.py
+++ polls/views.py
@@ -22,11 +22,49 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.POST = None
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(is_published=True)
+
+    def vote(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+
+        # Check if voting is allowed for this question
+        if not question.can_vote():
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "question": question,
+                    "error_message": "Voting for this question is not allowed.",
+                },
+            )
+
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        except (KeyError, Choice.DoesNotExist):
+            # Redisplay the question voting form.
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "question": question,
+                    "error_message": "You didn't select a choice.",
+                },
+            )
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 class ResultsView(generic.DetailView):
@@ -36,10 +74,22 @@ class ResultsView(generic.DetailView):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
+    # Check if voting is allowed for this question
+    if not question.can_vote():
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Voting for this question is not allowed.",
+            },
+        )
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+        # Re-show the voting form for the question.
         return render(
             request,
             "polls/detail.html",
@@ -51,7 +101,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+        # Use HttpResponseRedirect to prevent duplicate form submissions when users click the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
